6bb36ee901235c730a63e44e78fce6bade56ceac
Pratik Sharma <sharma.pratik2016@gmail.com>
2023-02-22 23:20:19 +0530

    bug fix: screen goes blank

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -1,38 +1,3 @@
-const schema = {
-  "@context": "https://schema.org",
-  "@type": "SoftwareApplication",
-  applicationCategory: "WebApplication",
-
-  name: "Webstory code generator",
-  mainEntityOfPage: "webstory.coolhead.in",
-  headline:
-    "Web story code generator - no code tool to create google web stories ",
-  description:
-    "Free, visual no code editor to create google web stories from your blogs - hashnode, medium and dev.to",
-  author: {
-    "@type": "Person",
-    name: "Pratik Sharma",
-    // The full URL must be provided, including the website's domain.
-    url: "https://webstory.coolhead.in",
-  },
-  screenshot: {
-    "@type": "ImageObject",
-    url: "https://webstory.coolhead.in/ogimage.png",
-  },
-  aggregateRating: {
-    "@type": "AggregateRating",
-    ratingValue: "4.6",
-    ratingCount: "8864",
-  },
-
-  image: {
-    "@type": "ImageObject",
-    url: "https://webstory.coolhead.in/ogimage.png",
-  },
-
-  isAccessibleForFree: "http://schema.org/True",
-};
-
 export default function Head() {
   return (
     <>
@@ -71,10 +36,6 @@ export default function Head() {
 
       <meta name="twitter:creator" content="@biomathcode" />
       <link rel="icon" href="/favicon.ico" />
-      <script
-        type="application/ld+json"
-        dangerouslySetInnerHTML={{ __html: JSON.stringify(schema) }}
-      />
     </>
   );
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,40 @@
+import Script from "next/script";
 import "./globals.css";
+
+const schema = {
+  "@context": "https://schema.org",
+  "@type": "SoftwareApplication",
+  applicationCategory: "WebApplication",
+
+  name: "Webstory code generator",
+  mainEntityOfPage: "webstory.coolhead.in",
+  headline:
+    "Web story code generator - no code tool to create google web stories ",
+  description:
+    "Free, visual no code editor to create google web stories from your blogs - hashnode, medium and dev.to",
+  author: {
+    "@type": "Person",
+    name: "Pratik Sharma",
+    // The full URL must be provided, including the website's domain.
+    url: "https://webstory.coolhead.in",
+  },
+  screenshot: {
+    "@type": "ImageObject",
+    url: "https://webstory.coolhead.in/ogimage.png",
+  },
+  aggregateRating: {
+    "@type": "AggregateRating",
+    ratingValue: "4.6",
+    ratingCount: "8864",
+  },
+
+  image: {
+    "@type": "ImageObject",
+    url: "https://webstory.coolhead.in/ogimage.png",
+  },
+
+  isAccessibleForFree: "http://schema.org/True",
+};
 
 export default function RootLayout({
   children,
@@ -8,6 +44,13 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head />
+
+      <Script
+        id="this"
+        type="application/ld+json"
+        dangerouslySetInnerHTML={{ __html: JSON.stringify(schema) }}
+      />
+
       <body>{children}</body>
     </html>
   );
