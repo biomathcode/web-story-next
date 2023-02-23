154c2b5a552c06c47cbf5c822f281743b581af2c
Pratik Sharma <sharma.pratik2016@gmail.com>
2023-02-23 20:38:52 +0530

    update privacy policies

---
+++ src/pages/How-to-monetize-web-stories.tsx
@@ -0,0 +1,25 @@
+import Head from "next/head";
+
+function Monetization() {
+  return (
+    <div>
+      <Head>
+        <title>How to monetize web stories ?</title>
+      </Head>
+      <div className="flex center col jc gap-10 inter p-20 m-20">
+        <div
+          className="flex col gap-10 inter"
+          style={{ maxWidth: "600px", margin: "20px", padding: "20px" }}
+        >
+          <h1>How to monetize web stories </h1>
+          <ul>
+            <li>Connect your site to google adsense</li>
+            <li></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Monetization;
