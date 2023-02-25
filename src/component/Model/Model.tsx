b4dd5b4f8db2c20052de8d576a9af9dc83c32f57
Pratik Sharma <sharma.pratik2016@gmail.com>
2023-02-25 18:13:40 +0530

    deployment fix

--- src/component/Model/Model.tsx
+++ src/component/Model/Model.tsx
@@ -109,7 +109,8 @@ const Model = () => {
       <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link href="https://fonts.googleapis.com/css2?family=Inter&display=swap" rel="stylesheet"></link>
-      `
+      `,
+      ""
     );
 
     setCode(newData);
