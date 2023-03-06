1f9a8161940cddb8ccfbfbb2c2fbd361e9eabb2c
Pratik Sharma <sharma.pratik2016@gmail.com>
2023-03-06 22:05:19 +0530

    added web story

---
+++ src/pages/10-mistakes-of-entrepreneurs-guy-kawasaki-coolhead-webstory.jsx
@@ -0,0 +1,512 @@
+import Head from "next/head";
+
+export const config = {
+  amp: true,
+};
+
+export default function IndexPage() {
+  return (
+    <>
+      <Head>
+        <title>
+          10 Mistakes of Entrepreneurs - Guy Kawasaki - Coolhead Webstory
+        </title>
+        <meta
+          name="description"
+          content="10 Mistakes made by founder while starting a early stage startup. Solutions to founder problems. Advice by Guy Kawasaki"
+        />
+        <meta property="og:type" content="website" />
+        <meta
+          property="og:title"
+          content="10 Mistakes of Entrepreneurs - Guy Kawasaki - Coolhead Webstory"
+        />
+        <meta
+          property="og:image"
+          content="https://i.ytimg.com/vi/HHjgK6p4nrw/hqdefault_968833.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCRWy9k9RG05qtyILxNf5ZWvQMlPA"
+        />
+
+        <meta
+          property="og:description"
+          content="10 Mistakes made by founder while starting a early stage startup. Solutions to founder problems. Advice by Guy Kawasaki"
+        />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="true"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Inter&display=swap"
+          rel="stylesheet"
+        ></link>
+        {/* <link
+          rel="canonical"
+          href="https://webstory.coolhead.in/10-mistakes-of-entrepreneurs-guy-kawasaki-coolhead-webstory"
+        /> */}
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{
+            __html: JSON.stringify({
+              "@context": "https://schema.org",
+              "@type": "NewsArticle",
+              mainEntityOfPage:
+                "10-mistakes-of-entrepreneurs-guy-kawasaki-coolhead-webstory",
+              headline:
+                "10 Mistakes of Entrepreneurs - Guy Kawasaki - Coolhead Webstory",
+              description:
+                "10 Mistakes made by founder while starting a early stage startup. Solutions to founder problems. Advice by Guy Kawasaki",
+              author: {
+                "@type": "Person",
+                name: "Pratik Sharma",
+                url: "http://coolhead.in",
+              },
+              publisher: {
+                "@type": "Organization",
+                name: "Coolhead",
+                url: "https://webstory.coolhead.in",
+              },
+              image: {
+                "@type": "ImageObject",
+                url: "https://i.ytimg.com/vi/HHjgK6p4nrw/hqdefault_968833.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCRWy9k9RG05qtyILxNf5ZWvQMlPA",
+              },
+              datePublished: "2023-03-06T16:21:17.237Z",
+              dateModified: "2023-03-06T16:21:17.237Z",
+              isAccessibleForFree: "http://schema.org/True",
+            }),
+          }}
+          key="product-jsonld"
+        />
+      </Head>
+
+      <style jsx>
+        {`
+          .overlay {
+            width: 100%;
+            height: 100%;
+            background: linear-gradient(to bottom, transparent 0%, black 100%);
+          }
+        `}
+      </style>
+
+      <amp-story
+        standalone=""
+        title="10 Mistakes of Entrepreneurs - Guy Kawasaki - Coolhead Webstory"
+        publisher="Coolhead"
+        publisher-logo-src="https://webstory.coolhead.in/apple-icon.png"
+        poster-portrait-src="https://images.unsplash.com/photo-1529535117092-eaaa1c978fc7?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=Mnw0MDYxOTZ8MHwxfHNlYXJjaHw0fHxFbnRyZXByZW5ldXJzfGVufDB8MXx8fDE2NzgxMTc1NDk&ixlib=rb-4.0.3&q=80&w=400"
+      >
+        <amp-story-page id="0">
+          <amp-story-grid-layer template="fill">
+            <amp-img
+              src="https://images.unsplash.com/photo-1529535117092-eaaa1c978fc7?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=Mnw0MDYxOTZ8MHwxfHNlYXJjaHw0fHxFbnRyZXByZW5ldXJzfGVufDB8MXx8fDE2NzgxMTc1NDk&ixlib=rb-4.0.3&q=80&w=400"
+              width="360"
+              height="720"
+              layout="fill"
+            ></amp-img>
+          </amp-story-grid-layer>
+
+          <amp-story-grid-layer template="fill">
+            <div className="overlay"></div>
+          </amp-story-grid-layer>
+
+          <amp-story-grid-layer template="vertical">
+            <h1
+              style={{
+                color: "rgba(255, 255, 255,1)",
+                background: "rgba(0, 0, 0,0.6)",
+                position: "absolute",
+                top: "57%",
+                fontSize: "25px",
+                padding: "14px 8px",
+                textAlign: "center",
+                lineHeight: "78px",
+                fontFamily: "Inter",
+              }}
+            >
+              10 Mistakes Entrepreneurs make - by Guy Kawasaki
+            </h1>
+          </amp-story-grid-layer>
+        </amp-story-page>
+
+        <amp-story-page id="1">
+          <amp-story-grid-layer template="fill">
+            <amp-img
+              src="https://images.unsplash.com/photo-1579532537598-459ecdaf39cc?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=Mnw0MDYxOTZ8MHwxfHNlYXJjaHwxfHxtYXJrZXQlMjBzaGFyZXxlbnwwfDF8fHwxNjc4MTE4MDM5&ixlib=rb-4.0.3&q=80&w=400"
+              width="360"
+              height="720"
+              layout="fill"
+              animate-in="fade-in"
+            ></amp-img>
+          </amp-story-grid-layer>
+
+          <amp-story-grid-layer template="fill">
+            <div className="overlay"></div>
+          </amp-story-grid-layer>
+
+          <amp-story-grid-layer template="vertical">
+            <p
+              style={{
+                color: "rgba(255, 255, 255,1)",
+                background: "rgba(0, 0, 0,0.6)",
+                position: "absolute",
+                top: "57%",
+                fontSize: "25px",
+                padding: "14px 8px",
+                textAlign: "center",
+                lineHeight: "78px",
+                fontFamily: "Inter",
+              }}
+              animate-in="fly-in-bottom"
+            >
+              1. Multiply big numbers by 1 percent. Solution: Calculate from
+              bottom up.
+            </p>
+          </amp-story-grid-layer>
+        </amp-story-page>
+
+        <amp-story-page id="2">
+          <amp-story-grid-layer template="fill">
+            <amp-img
+              src="https://images.unsplash.com/photo-1533162507191-d90c625b2640?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=Mnw0MDYxOTZ8MHwxfHNlYXJjaHw5fHxzY2FsZXxlbnwwfDF8fHwxNjc4MTE4MjM2&ixlib=rb-4.0.3&q=80&w=400"
+              width="360"
+              height="720"
+              layout="fill"
+              animate-in="fade-in"
+            ></amp-img>
+          </amp-story-grid-layer>
+
+          <amp-story-grid-layer template="fill">
+            <div className="overlay"></div>
+          </amp-story-grid-layer>
+
+          <amp-story-grid-layer template="vertical">
+            <p
+              style={{
+                color: "rgba(255, 255, 255,1)",
+                background: "rgba(0, 0, 0,0.6)",
+                position: "absolute",
+                top: "57%",
+                fontSize: "25px",
+                padding: "14px 8px",
+                textAlign: "center",
+                lineHeight: "78px",
+                fontFamily: "Inter",
+              }}
+              animate-in="fly-in-bottom"
+            >
+              2. Scale too fast.‎‎‎ ‎‎‎‎‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ Solution: Eat what
+              you kill.
+            </p>
+          </amp-story-grid-layer>
+        </amp-story-page>
+
+        <amp-story-page id="3">
+          <amp-story-grid-layer template="fill">
+            <amp-img
+              src="https://images.unsplash.com/photo-1517524285303-d6fc683dddf8?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=Mnw0MDYxOTZ8MHwxfHNlYXJjaHwyfHx3YWxsJTIwc3RyZWV0JTIwfGVufDB8MXx8fDE2NzgxMTgzNDU&ixlib=rb-4.0.3&q=80&w=400"
+              width="360"
+              height="720"
+              layout="fill"
+              animate-in="fade-in"
+            ></amp-img>
+          </amp-story-grid-layer>
+
+          <amp-story-grid-layer template="fill">
+            <div className="overlay"></div>
+          </amp-story-grid-layer>
+
+          <amp-story-grid-layer template="vertical">
+            <p
+              style={{
+                color: "rgba(255, 255, 255,1)",
+                background: "rgba(0, 0, 0,0.6)",
+                position: "absolute",
+                top: "57%",
+                fontSize: "25px",
+                padding: "14px 8px",
+                textAlign: "center",
+                lineHeight: "78px",
+                fontFamily: "Inter",
+              }}
+              animate-in="fly-in-bottom"
+            >
+              3. Focus on partnerships. Solution: Focus on sales.
+            </p>
+          </amp-story-grid-layer>
+        </amp-story-page>
+
+        <amp-story-page id="4">
+          <amp-story-grid-layer template="fill">
+            <amp-img
+              src="https://images.unsplash.com/photo-1577415124269-fc1140a69e91?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=Mnw0MDYxOTZ8MHwxfHNlYXJjaHw1fHxtZWV0aW5nfGVufDB8MXx8fDE2NzgxMTg1MDM&ixlib=rb-4.0.3&q=80&w=400"
+              width="360"
+              height="720"
+              layout="fill"
+              animate-in="fade-in"
+            ></amp-img>
+          </amp-story-grid-layer>
+
+          <amp-story-grid-layer template="fill">
+            <div className="overlay"></div>
+          </amp-story-grid-layer>
+
+          <amp-story-grid-layer template="vertical">
+            <p
+              style={{
+                color: "rgba(255, 255, 255,1)",
+                background: "rgba(0, 0, 0,0.6)",
+                position: "absolute",
+                top: "57%",
+                fontSize: "25px",
+                padding: "14px 8px",
+                textAlign: "center",
+                lineHeight: "78px",
+                fontFamily: "Inter",
+              }}
+              animate-in="fly-in-bottom"
+            >
+              4. Focus on the pitch. ‎‎‎ ‎ ‎ ‎ ‎ ‎ ‎ Solution: Focus on the
+              prototype.
+            </p>
+          </amp-story-grid-layer>
+        </amp-story-page>
+
+        <amp-story-page id="5">
+          <amp-story-grid-layer template="fill">
+            <amp-img
+              src="https://images.unsplash.com/photo-1544535830-9df3f56fff6a?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=Mnw0MDYxOTZ8MHwxfHNlYXJjaHw4fHxwcmVzZW50YXRpb258ZW58MHwxfHx8MTY3ODExODYzMg&ixlib=rb-4.0.3&q=80&w=400"
+              width="360"
+              height="720"
+              layout="fill"
+              animate-in="fade-in"
+            ></amp-img>
+          </amp-story-grid-layer>
+
+          <amp-story-grid-layer template="fill">
+            <div className="overlay"></div>
+          </amp-story-grid-layer>
+
+          <amp-story-grid-layer template="vertical">
+            <p
+              style={{
+                color: "rgba(255, 255, 255,1)",
+                background: "rgba(0, 0, 0,0.6)",
+                position: "absolute",
+                top: "57%",
+                fontSize: "25px",
+                padding: "14px 8px",
+                textAlign: "center",
+                lineHeight: "78px",
+                fontFamily: "Inter",
+              }}
+              animate-in="fly-in-bottom"
+            >
+              6. Use too many slides. Solution: 10 slides, focus on story
+            </p>
+          </amp-story-grid-layer>
+        </amp-story-page>
+
+        <amp-story-page id="6">
+          <amp-story-grid-layer template="fill">
+            <amp-img
+              src="https://images.unsplash.com/photo-1554595666-19ceabf46a84?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80"
+              width="360"
+              height="720"
+              layout="fill"
+              animate-in="fade-in"
+            ></amp-img>
+          </amp-story-grid-layer>
+
+          <amp-story-grid-layer template="fill">
+            <div className="overlay"></div>
+          </amp-story-grid-layer>
+
+          <amp-story-grid-layer template="vertical">
+            <p
+              style={{
+                color: "rgba(255, 255, 255,1)",
+                background: "rgba(0, 0, 0,0.6)",
+                position: "absolute",
+                top: "57%",
+                fontSize: "25px",
+                padding: "14px 20px",
+                textAlign: "left",
+                lineHeight: "78px",
+                fontFamily: "Inter",
+              }}
+              animate-in="fly-in-bottom"
+            >
+              6. Proceed serially. ‎‎‎ ‎ ‎ ‎ ‎ Solution: Life is parallel.
+            </p>
+          </amp-story-grid-layer>
+        </amp-story-page>
+
+        <amp-story-page id="7">
+          <amp-story-grid-layer template="fill">
+            <amp-img
+              src="https://images.unsplash.com/photo-1519915028121-7d3463d20b13?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=Mnw0MDYxOTZ8MHwxfHNlYXJjaHw4fHxwaWUlMjB8ZW58MHwxfHx8MTY3ODExODgzNA&ixlib=rb-4.0.3&q=80&w=400"
+              width="360"
+              height="720"
+              layout="fill"
+              animate-in="fade-in"
+            ></amp-img>
+          </amp-story-grid-layer>
+
+          <amp-story-grid-layer template="fill">
+            <div className="overlay"></div>
+          </amp-story-grid-layer>
+
+          <amp-story-grid-layer template="vertical">
+            <p
+              style={{
+                color: "rgba(255, 255, 255,1)",
+                background: "rgba(0, 0, 0,0.6)",
+                position: "absolute",
+                top: "57%",
+                fontSize: "25px",
+                padding: "14px 30px",
+                textAlign: "left",
+                lineHeight: "78px",
+                fontFamily: "Inter",
+              }}
+              animate-in="fly-in-bottom"
+            >
+              7. Retain control. ‎‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ Solution: Make a bigger pie.
+            </p>
+          </amp-story-grid-layer>
+        </amp-story-page>
+
+        <amp-story-page id="8">
+          <amp-story-grid-layer template="fill">
+            <amp-img
+              src="https://images.unsplash.com/photo-1608197492882-4ab08b61f78c?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=Mnw0MDYxOTZ8MHwxfHNlYXJjaHwxfHxwYXRlbnR8ZW58MHwxfHx8MTY3ODExODkwMw&ixlib=rb-4.0.3&q=80&w=400"
+              width="360"
+              height="720"
+              layout="fill"
+              animate-in="fade-in"
+            ></amp-img>
+          </amp-story-grid-layer>
+
+          <amp-story-grid-layer template="fill">
+            <div className="overlay"></div>
+          </amp-story-grid-layer>
+
+          <amp-story-grid-layer template="vertical">
+            <p
+              style={{
+                color: "rgba(255, 255, 255,1)",
+                background: "rgba(0, 0, 0,0.6)",
+                position: "absolute",
+                top: "57%",
+                fontSize: "25px",
+                padding: "14px 8px",
+                textAlign: "left",
+                lineHeight: "68px",
+                fontFamily: "Inter",
+              }}
+              animate-in="fly-in-bottom"
+            >
+              8. Use patents for defensibility. Solution: Use success for
+              defensibility Scale makes you defensible.
+            </p>
+          </amp-story-grid-layer>
+        </amp-story-page>
+
+        <amp-story-page id="9">
+          <amp-story-grid-layer template="fill">
+            <amp-img
+              src="https://images.unsplash.com/photo-1579389083395-4507e98b5e67?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=Mnw0MDYxOTZ8MHwxfHNlYXJjaHwzfHxoaXJpbmd8ZW58MHwxfHx8MTY3ODExOTEyNw&ixlib=rb-4.0.3&q=80&w=400"
+              width="360"
+              height="720"
+              layout="fill"
+              animate-in="fade-in"
+            ></amp-img>
+          </amp-story-grid-layer>
+
+          <amp-story-grid-layer template="fill">
+            <div className="overlay"></div>
+          </amp-story-grid-layer>
+
+          <amp-story-grid-layer template="vertical">
+            <p
+              style={{
+                color: "rgba(255, 255, 255,1)",
+                background: "rgba(0, 0, 0,0.6)",
+                position: "absolute",
+                top: "57%",
+                fontSize: "25px",
+                padding: "14px 18px",
+                textAlign: "left",
+                lineHeight: "78px",
+                fontFamily: "Inter",
+              }}
+              animate-in="fly-in-bottom"
+            >
+              9. Hire like you. ‎‎‎ ‎ ‎ ‎ ‎‎ ‎ ‎ ‎ ‎ ‎ ‎ Solution: Hire to
+              complement.
+            </p>
+          </amp-story-grid-layer>
+        </amp-story-page>
+
+        <amp-story-page id="10">
+          <amp-story-grid-layer template="fill">
+            <amp-img
+              src="https://images.unsplash.com/photo-1524475922556-171e86798684?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=Mnw0MDYxOTZ8MHwxfHNlYXJjaHwxfHxoaXJpbmd8ZW58MHwxfHx8MTY3ODExOTEyNw&ixlib=rb-4.0.3&q=80&w=400"
+              width="360"
+              height="720"
+              layout="fill"
+              animate-in="fade-in"
+            ></amp-img>
+          </amp-story-grid-layer>
+
+          <amp-story-grid-layer template="fill">
+            <div className="overlay"></div>
+          </amp-story-grid-layer>
+
+          <amp-story-grid-layer template="vertical">
+            <p
+              style={{
+                color: "rgba(255, 255, 255,1)",
+                background: "rgba(0, 0, 0,0.6)",
+                position: "absolute",
+                top: "60%",
+                fontSize: "23px",
+                padding: "14px 14px",
+                textAlign: "left",
+                lineHeight: "78px",
+                fontFamily: "Inter",
+              }}
+              animate-in="fly-in-bottom"
+            >
+              10. Befriend your investors. Solution: Exceed expectations.
+            </p>
+          </amp-story-grid-layer>
+
+          <amp-story-page-outlink layout="nodisplay">
+            <a
+              href="https://www.youtube.com/watch?v=HHjgK6p4nrw&t=3941s"
+              title="Link Description"
+            >
+              Watch Video
+            </a>
+          </amp-story-page-outlink>
+        </amp-story-page>
+
+        <amp-story-auto-ads>
+          <script
+            type="application/json"
+            dangerouslySetInnerHTML={{
+              __html: JSON.stringify({
+                "ad-attributes": {
+                  type: "adsense",
+                  "data-ad-client": "ca-pub-7971530262943091",
+                  "data-ad-slot": "2891356213",
+                },
+              }),
+            }}
+          ></script>
+        </amp-story-auto-ads>
+      </amp-story>
+    </>
+  );
+}
