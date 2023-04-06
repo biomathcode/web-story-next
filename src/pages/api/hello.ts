3b2b022ba1d8e27f4c0a51b64d6ff5fd7847fb1d
Pratik Sharma <sharma.pratik2016@gmail.com>
2023-04-06 20:51:22 +0530

    udpate

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,13 +1,36 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import mdParser from "@/lib/markdownParser";
+import { marked } from "marked";
+import type { NextApiRequest, NextApiResponse } from "next";
+import fetch from "node-fetch";
 
+import { convert } from "html-to-text";
 type Data = {
-  name: string
-}
+  data?: string;
+  head?: string;
+  body?: string;
+  content?: string;
+  text: string;
+};
 
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  const response = await fetch("https://blog.coolhead.in/contact");
+  const data = await response.text();
+
+  const head = data.match(/<head[^>]*>[\s\S]*<\/head>/gi)?.join("");
+  const body = data.match(/<body[^>]*>[\s\S]*<\/body>/gi)?.join(" ");
+
+  if (head && body) {
+    const content = marked.parse(body);
+
+    const newbody = marked.parse(body);
+
+    const text = convert(newbody, { wordwrap: 130 });
+
+    console.log(data);
+    res.status(200).json({ text: text });
+  }
 }
