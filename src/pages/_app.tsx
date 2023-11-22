3b27ba6d6d9a73937da0bd558ca87480cf914230
Pratik sharma <sharma.pratik2016@gmail.com>
2023-11-22 23:58:00 +0530

    added devrel widget

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,14 @@
 import type { AppProps } from "next/app";
 import Script from "next/script";
+import { useEffect } from "react";
 import "./globals.css";
 
 const App = ({ Component, pageProps }: AppProps) => {
+  useEffect(() => {
+    (window as any)?.plugSDK?.init({
+      app_id: "don:core:dvrv-us-1:devo/13GRGt258:plug_setting/1",
+    });
+  }, []);
   return (
     <>
       <Script
@@ -22,6 +28,14 @@ const App = ({ Component, pageProps }: AppProps) => {
         async
         src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7971530262943091"
         crossOrigin="anonymous"
+      ></Script>
+
+      <Script
+        id="devrev"
+        src="https://plug-platform.devrev.ai/static/plug.js"
+        type="text/javascript"
+        crossOrigin="anonymous"
+        strategy="beforeInteractive"
       ></Script>
       <Component {...pageProps} />
     </>
